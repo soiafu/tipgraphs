a3f3b030e3c7c6899a0ba45e28e77e74540b86a5
soiafu <su22@njit.edu>
2024-04-30 11:53:44 -0400

    set up d3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
+import React,{Component} from "react";
 import './App.css';
+import * as d3 from 'd3';
+import tips from "./tips.csv";
 
-function App() {
-  return (
-    <div className="App">
-      <div className="header">
-        <div className="header-text">Select Target:</div>
-        <select>
-          [replace options with actual input from csv]
-          <option value="option1">Option 1</option>
-          <option value="option2">Option 2</option>
-          <option value="option3">Option 3</option>
-        </select>
-      </div>
-      <div className="parent1"> 
+class App extends Component{
+  constructor(props){
+    super(props)
+    this.state={data:[]}
+  }
+  componentDidMount(){
+    var self=this
+    d3.csv(tips,function(d){
+      return {
+        tip:parseFloat(d.tip),
+        total_bill:parseFloat(d.total_bill),
+        day:d.day
+      }
+    }).then(function(csv_data){
+      self.setState({data:csv_data})
+      //console.log(csv_data)
+    })
+    .catch(function(err){
+      console.log(err)
+    })
+
+  }
+  render(){
+    return (
+      <div className="App">
+        <div className="header">
+          <div className="header-text">Select Target:</div>
+          <select>
+            [replace options with actual input from csv]
+            <option value="option1">Option 1</option>
+            <option value="option2">Option 2</option>
+            <option value="option3">Option 3</option>
+          </select>
+        </div>
+        <div className="parent1"> 
+          <div className="graph"></div>
+          <div className="graph"></div>
+        </div>
         <div className="graph"></div>
-        <div className="graph"></div>
       </div>
-      <div className="graph"></div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
