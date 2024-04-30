590cc9dd9beeb5402a5ee903dca04caa8e656c5d
soiafu <su22@njit.edu>
2024-04-29 21:54:45 -0400

    created html structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="header-text">Select Target:</div>
+        <select>
+          [replace options with actual input from csv]
+          <option value="option1">Option 1</option>
+          <option value="option2">Option 2</option>
+          <option value="option3">Option 3</option>
+        </select>
+      </div>
+      <div className="parent1"> 
+        <div className="graph"></div>
+        <div className="graph"></div>
+      </div>
+      <div className="graph"></div>
     </div>
   );
 }
