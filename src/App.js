2b8479f07bd51d95758d061988d669b1591c24cb
nYalla6 <ny6@njit.edu>
2024-04-30 15:48:56 -0400

    added the options necessary from the csv

--- src/App.js
+++ src/App.js
@@ -7,43 +7,54 @@ import * as d3 from 'd3';
 import tips from "./tips.csv";
 
 class App extends Component{
-  constructor(props){
-    super(props)
-    this.state={data:[]}
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      columns: [], // Initialize columns array
+      selectedTarget: null
+    };
   }
-  componentDidMount(){
-    var self=this
-    d3.csv(tips,function(d){
-      return {
-        tip:parseFloat(d.tip),
-        total_bill:parseFloat(d.total_bill),
-        day:d.day
-      }
-    }).then(function(csv_data){
-      self.setState({data:csv_data})
-      //console.log(csv_data)
-    })
-    .catch(function(err){
-      console.log(err)
-    })
+  
 
+  componentDidMount() {
+    const self = this;
+
+    d3.csv(tips, function (d) {
+      return {
+        tip: parseFloat(d.tip),
+        total_bill: parseFloat(d.total_bill),
+        day: d.day
+      };
+    }).then(function (csv_data) {
+      const columns = csv_data.length > 0 ? Object.keys(csv_data[0]) : [];
+      self.setState({ data: csv_data, columns: columns });
+    }).catch(function (err) {
+      console.log(err);
+    });
   }
-  render(){
+
+  handleTargetChange = (event) => {
+    const selectedTarget = event.target.value;
+    this.setState({ selectedTarget });
+  };
+
+  render() {
     return (
       <div className="App">
         <div className="header">
           <div className="header-text">Select Target:</div>
-          <select>
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
+          <select onChange={this.handleTargetChange}>
+            {this.state.columns.map((column, index) => (
+              <option key={index} value={column}>{column}</option>
+            ))}
           </select>
         </div>
-        <div className="parent1"> 
-          <div className="graph"><Graph1 data1={this.state.data}></Graph1></div>
-          <div className="graph"><Graph2 data2={this.state.data}></Graph2></div>
+        <div className="parent1">
+          <div className="graph"><Graph1 data1={this.state.data} selectedTarget={this.state.selectedTarget}></Graph1></div>
+          <div className="graph"><Graph2 data2={this.state.data} selectedTarget={this.state.selectedTarget}></Graph2></div>
         </div>
-        <div className="graph"><Graph3 data3={this.state.data}></Graph3></div>
+        <div className="graph"><Graph3 data3={this.state.data} selectedTarget={this.state.selectedTarget}></Graph3></div>
       </div>
     );
   }
